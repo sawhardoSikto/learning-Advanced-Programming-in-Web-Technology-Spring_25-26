4e8d7a4642b0c251f94f688fa03b0e338489bedb
Sawhardo Biswas Sikto <22-47406-2@student.aiub.edu>
2026-04-02 23:48:15 +0600

    crud sone

--- type-orm/src/users/users.controller.ts
+++ type-orm/src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Controller, Get, UploadedFile } from '@nestjs/common';
+import { BadRequestException, Controller, Delete,Put, Get, Param, ParseIntPipe, Patch, UploadedFile } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Post, UseInterceptors,Body } from '@nestjs/common';
 
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { CreateUserDto } from 'src/DTOs/cerate-user.dto';
+import { UpdateUserDto } from 'src/DTOs/update-user.dto';
 
 
 @Controller('users')
@@ -48,4 +49,52 @@ createUser(
     getAllUsers() {
         return this.usersService.getAll();
     }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: number) {
+        return this.usersService.delete(id);
+    }
+
+    @Put(':id')
+    @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const filename = Date.now() + '-' + file.originalname;
+          cb(null, filename);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (file.originalname.match(/^.*\.(jpg|jpeg|pdf)$/)) {
+          cb(null, true);
+        } else {
+          cb(
+            new BadRequestException('only image files and pdf are accepted'),
+            false,
+          );
+        }
+      },
+
+      limits: {
+        fileSize: 3 * 1024 * 1024,
+      },
+    }),
+  )
+ updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.usersService.update( id,updateUserDto, file.filename);
+  }
+
+  @Patch(':id')
+partialUpdateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() partialUpdateUserDto: Partial<UpdateUserDto>,
+  ) {
+    return this.usersService.partialUpdate(id, partialUpdateUserDto);
+  }
+
 }
