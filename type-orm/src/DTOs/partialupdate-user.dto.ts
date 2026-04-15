6da7477a10ce6a53980a5701a2a0d88ec19d4265
Sawhardo Biswas Sikto <22-47406-2@student.aiub.edu>
2026-04-15 21:56:51 +0600

    4 lab task completed

--- type-orm/src/DTOs/partialupdate-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateUserDto } from "./cerate-user.dto";
-
-export class PartialUpdateUserDto extends PartialType(CreateUserDto) {}
--- type-orm/src/DTOs/update-user.dto.ts
+++
@@ -1,3 +0,0 @@
-
-import { CreateUserDto } from './cerate-user.dto';
-export class UpdateUserDto extends CreateUserDto {}
